b662d479499456c6e7ea75c843f1d4423bfb6b71
Erick Birbe <erickcion@gmail.com>
2016-03-10 02:22:07 -0430

    Add module calculator.py with sum method

--- calculator.py
+++ calculator.py
@@ -1,16 +1,32 @@
 """
-Your module documentation here
+calculator.py - Calculator module for vauxoo-applicant
+Author: Erick Birbe <erickcion@gmail.com>
+Date: 2015-03-09
+
+Calculator is a python module with useful classes and methods to make diffrent
+mathematic operations.
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    CalculatorClass makes easy to implement classical mathematic operation into
+    your code.
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        sum method helps you to make addition operations over several numbers
+        with a single call from your code.
+
+        Method Arguments:
+        - num_list: A list of integer to sum.
+
+        Return Value:
+        The total sum of all the num_list values.
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+
+        total = 0
+        for val in num_list:
+            total += val
+        return total
