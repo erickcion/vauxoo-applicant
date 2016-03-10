0b3fffcf091788f06c680db1a418f9a98f42214c
Erick Birbe <erickcion@gmail.com>
2016-03-09 19:45:15 -0430

    Create employee and department tables

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -1,15 +1,43 @@
--- Your sql code in this file
--- NOTE: Please, don't add sentence to create database in this script file.
---       You can create database locally to test it.
---       Consider add ';' at end sentence.
-
-CREATE TABLE employee (
+-- Table employee
+CREATE TABLE employee
+(
+   id serial, 
+   first_name character varying, 
+   last_name character varying, 
+   PRIMARY KEY (id)
 );
 
-CREATE TABLE employee_department (
+-- Table employee_department
+CREATE TABLE employee_department
+(
+   id serial, 
+   name character varying, 
+   description character varying, 
+   PRIMARY KEY (id)
 );
 
-CREATE TABLE employee_hobby (
-);
+-- Foreign Key
+ALTER TABLE employee
+  ADD COLUMN department_id integer;
+ALTER TABLE employee
+  ADD FOREIGN KEY (department_id) REFERENCES employee_department (id);
 
--- ...
+
+-- Insert departments
+INSERT INTO employee_department(name, description)
+    VALUES ('Reception', 'Client information and reception area'),
+	   ('Administration', 'Administration and cash department'),
+	   ('Human Resources', 'Direct interaction with employees and contract activities'),
+	   ('Electrical Coordination', 'Projects electrical team'),
+	   ('Planning', 'Project planing team'),
+	   ('Direction', 'General direction of the company')
+;
+
+-- Insert employees
+INSERT INTO employee(
+            first_name, last_name, department_id)
+    VALUES ('Sara', 'Díaz', 6),
+		   ('Erick', 'Birbe', 4),
+		   ('Juan', 'Contreras', 4),
+		   ('Belkys', 'Salazar', 1)
+;
